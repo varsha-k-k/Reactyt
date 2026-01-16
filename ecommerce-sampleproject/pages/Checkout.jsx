3a38a1be892d7629b96761b8c896930c82c4db95
vrshaa66 <varshakk2005@gmail.com>
2026-01-16 21:08:27 +0530

    generate html for the delivery options for each cart item

--- ecommerce-sampleproject/pages/Checkout.jsx
+++ ecommerce-sampleproject/pages/Checkout.jsx
@@ -1,10 +1,22 @@
-import'./checkout.css';
-import'./checkout-header.css'; 
-import {convertCenttoDollar} from "../src/utils/money";
-export function Checkout({cart}) {
+import axios from "axios";
+import dayjs from "dayjs";
+import { useState, useEffect } from "react";
+import "./checkout.css";
+import "./checkout-header.css";
+import { convertCenttoDollar } from "../src/utils/money";
+export function Checkout({ cart }) {
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/delivery-options?expand=estimatedDeliveryTime")
+      .then((response) => {
+        setDeliveryOptions(response.data);
+      });
+  }, []);
   return (
     <>
-    <title>Checkout</title>
+      <title>Checkout</title>
       <div>
         <div className="checkout-header">
           <div className="header-content">
@@ -34,40 +46,82 @@ export function Checkout({cart}) {
 
           <div className="checkout-grid">
             <div className="order-summary">
-              {cart.map((item)=>
-              {
-                  return (    <div  key={item.productId}   className="cart-item-container">
-                <div className="delivery-date">Delivery date: Tuesday, June 21</div>
+              {deliveryOptions.length > 0 &&
+                cart.map((item) => {
+                  const selectedOption = deliveryOptions.find((option) => {
+                    return option.id == item.deliveryOptionId;
+                  });
+                  return (
+                    <div key={item.productId} className="cart-item-container">
+                      <div className="delivery-date">
+                        Delivery date:
+                        {dayjs(selectedOption.estimatedDeliveryTimeMs).format(
+                          "dddd, MMMM D"
+                        )}
+                      </div>
 
-                <div className="cart-item-details-grid">
-                  <img
-                    className="product-image"
-                    src={item.product.image}
-                  />
+                      <div className="cart-item-details-grid">
+                        <img
+                          className="product-image"
+                          src={item.product.image}
+                        />
 
-                  <div className="cart-item-details">
-                    <div className="product-name">
-                      {item.product.name}
-                    </div>
-                    <div className="product-price">{convertCenttoDollar(item.product.priceCents)}</div>
-                    <div className="product-quantity">
-                      <span>
-                        Quantity: <span className="quantity-label">{item.quantity}</span>
-                      </span>
-                      <span className="update-quantity-link link-primary">
-                        Update
-                      </span>
-                      <span className="delete-quantity-link link-primary">
-                        Delete
-                      </span>
-                    </div>
-                  </div>
+                        <div className="cart-item-details">
+                          <div className="product-name">
+                            {item.product.name}
+                          </div>
+                          <div className="product-price">
+                            {convertCenttoDollar(item.product.priceCents)}
+                          </div>
+                          <div className="product-quantity">
+                            <span>
+                              Quantity:{" "}
+                              <span className="quantity-label">
+                                {item.quantity}
+                              </span>
+                            </span>
+                            <span className="update-quantity-link link-primary">
+                              Update
+                            </span>
+                            <span className="delete-quantity-link link-primary">
+                              Delete
+                            </span>
+                          </div>
+                        </div>
 
-                  <div className="delivery-options">
-                    <div className="delivery-options-title">
-                      Choose a delivery option:
-                    </div>
-                    <div className="delivery-option">
+                        <div className="delivery-options">
+                          <div className="delivery-options-title">
+                            Choose a delivery option:
+                          </div>
+                          {deliveryOptions.map((option) => {
+                            let priceString = "Free Shipping";
+                            if (option.priceCents > 0) {
+                              priceString = `${convertCenttoDollar(
+                                option.priceCents
+                              )} - shipping`;
+                            }
+                            return (
+                              <div key={option.id} className="delivery-option">
+                                <input
+                                  type="radio"
+                                  checked={option.id === item.deliveryOptionId}
+                                  className="delivery-option-input"
+                                  name={`delivery-option-${item.productId}`}
+                                />
+                                <div>
+                                  <div className="delivery-option-date">
+                                    {dayjs(
+                                      option.estimatedDeliveryTimeMs
+                                    ).format("dddd, MMMM D")}
+                                  </div>
+                                  <div className="delivery-option-price">
+                                    {priceString}
+                                  </div>
+                                </div>
+                              </div>
+                            );
+                          })}
+                          {/* <div className="delivery-option">
                       <input
                         type="radio"
                         checked
@@ -78,42 +132,13 @@ export function Checkout({cart}) {
                         <div className="delivery-option-date">Tuesday, June 21</div>
                         <div className="delivery-option-price">FREE Shipping</div>
                       </div>
-                    </div>
-                    <div className="delivery-option">
-                      <input
-                        type="radio"
-                        className="delivery-option-input"
-                        name="delivery-option-1"
-                      />
-                      <div>
-                        <div className="delivery-option-date">
-                          Wednesday, June 15
-                        </div>
-                        <div className="delivery-option-price">
-                          $4.99 - Shipping
+                    </div> */}
                         </div>
                       </div>
                     </div>
-                    <div className="delivery-option">
-                      <input
-                        type="radio"
-                        className="delivery-option-input"
-                        name="delivery-option-1"
-                      />
-                      <div>
-                        <div className="delivery-option-date">Monday, June 13</div>
-                        <div className="delivery-option-price">
-                          $9.99 - Shipping
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>);
-              })};
-       
-
-          
+                  );
+                })}
+              ;
             </div>
 
             <div className="payment-summary">
