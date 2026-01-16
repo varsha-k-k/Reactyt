aa0e65bfd4f973580d6a46157848e026606ab4fd
vrshaa66 <varshakk2005@gmail.com>
2026-01-16 21:33:13 +0530

    generate html for payment summary

--- ecommerce-sampleproject/pages/Checkout.jsx
+++ ecommerce-sampleproject/pages/Checkout.jsx
@@ -6,6 +6,7 @@ import "./checkout-header.css";
 import { convertCenttoDollar } from "../src/utils/money";
 export function Checkout({ cart }) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
+  const [payment, setPayment] = useState(null);
 
   useEffect(() => {
     axios
@@ -13,6 +14,9 @@ export function Checkout({ cart }) {
       .then((response) => {
         setDeliveryOptions(response.data);
       });
+    axios.get("/api/payment-summary").then((response) => {
+      setPayment(response.data);
+    });
   }, []);
   return (
     <>
@@ -143,35 +147,49 @@ export function Checkout({ cart }) {
 
             <div className="payment-summary">
               <div className="payment-summary-title">Payment Summary</div>
+              {payment && (
+                <>
+                  <div className="payment-summary-row">
+                    <div>Items ({payment.totalItems}):</div>
+                    <div className="payment-summary-money">
+                      {convertCenttoDollar(payment.productCostCents)}
+                    </div>
+                  </div>
 
-              <div className="payment-summary-row">
-                <div>Items (3):</div>
-                <div className="payment-summary-money">$42.75</div>
-              </div>
+                  <div className="payment-summary-row">
+                    <div>Shipping &amp; handling:</div>
+                    <div className="payment-summary-money">
+                      {convertCenttoDollar(payment.shippingCostCents)}
+                    </div>
+                  </div>
 
-              <div className="payment-summary-row">
-                <div>Shipping &amp; handling:</div>
-                <div className="payment-summary-money">$4.99</div>
-              </div>
+                  <div className="payment-summary-row subtotal-row">
+                    <div>Total before tax:</div>
+                    <div className="payment-summary-money">
+                      {convertCenttoDollar(payment.totalCostBeforeTaxCents)}
+                    </div>
+                  </div>
 
-              <div className="payment-summary-row subtotal-row">
-                <div>Total before tax:</div>
-                <div className="payment-summary-money">$47.74</div>
-              </div>
+                  <div className="payment-summary-row">
+                    <div>Estimated tax (10%):</div>
+                    <div className="payment-summary-money">
+                      {" "}
+                      {convertCenttoDollar(payment.taxCents)}
+                    </div>
+                  </div>
 
-              <div className="payment-summary-row">
-                <div>Estimated tax (10%):</div>
-                <div className="payment-summary-money">$4.77</div>
-              </div>
+                  <div className="payment-summary-row total-row">
+                    <div>Order total:</div>
+                    <div className="payment-summary-money">
+                      {convertCenttoDollar(payment.totalCostCents)}
+                    </div>
+                  </div>
 
-              <div className="payment-summary-row total-row">
-                <div>Order total:</div>
-                <div className="payment-summary-money">$52.51</div>
-              </div>
-
-              <button className="place-order-button button-primary">
-                Place your order
-              </button>
+                  <button className="place-order-button button-primary">
+                    Place your order
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
